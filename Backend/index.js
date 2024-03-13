b2706c294a17f17359262d6830ecf7118a074e3c
Asif Saif Ali <asif263848@gmail.com>
2024-03-14 00:00:20 +0530

    inserted data into databases

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,12 @@
-const express = requrie('express')
+const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const {  createTodo, updateTodo} = require('./types')
-
+const Todo = require('./databases')
+const connection = require("./connection")
 app.use(bodyParser.json())
 
-app.post('/todo',(req,res)=>{
+app.post('/todo',async(req,res)=>{
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload)
     if(!parsedPayload.success){
@@ -13,23 +14,40 @@ app.post('/todo',(req,res)=>{
             msg : "You sent the wrong data"
         });
     }else{
+        const data = await Todo.create(req.body)
         return res.status(200).json({
             msg : "Successfully created todo",
             id: parsedPayload.data.id,
+            data : data,
             success  : true
         })
     }
     
 })
 
-app.get('/',(req,res)={
+app.get('/',(req,res)=>{
 
+})
+
+app.patch('/update/:id',(req,res)=>{
+    const parsedPay = req.body
+    const UpdatedPayload = updateTodo.safeParse(parsedPay)
+    if(!UpdatedPayload.success){
+        res.status(411).json({
+            msg : "Invalid payload",
+            success : false,
+
+        })
+    }
 })
 
 
 const PORT = 8000
 const Server = ()=>{
-    app.listen(8000, ()=>{
+    app.listen(8000, async()=>{
         console.log("Server is running on PORT",PORT);
+        await connection()
     })
 }
+
+Server()
